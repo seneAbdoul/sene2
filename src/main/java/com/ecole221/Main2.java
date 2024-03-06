252dd967568f55019d1ea4a496fac05df9b6a716
seneAbdoul <157800035+seneAbdoul@users.noreply.github.com>
2024-03-06 10:00:51 +0000

    Update Main2.java

--- src/main/java/com/ecole221/Main2.java
+++ src/main/java/com/ecole221/Main2.java
@@ -60,7 +60,6 @@ public class Main2 {
             }
             else{
                 System.out.println("Cette classe existe déjà");
-                System.out.println("Cette classe existe déjà zefsdgjhklj");
               }
         } catch (Exception e) {
             System.out.println(e);
